29653b21654a08f5ca6181e26c06672496484842
Balladie <47603474+Balladie@users.noreply.github.com>
2019-12-18 15:13:08 +0900

    Add files via upload

---
+++ 2019/Introduction to Data Structure/HW1/1/HW1_NaryBigNum_Test.cpp
@@ -0,0 +1,27 @@
+#include "HW1_NaryBigNum.h"
+
+int main() {
+	NaryBigNum a(16, "3G@36");
+	NaryBigNum b(16, "c6c77");
+	NaryBigNum c(8, "1941");
+	NaryBigNum d(8, "1554A12");
+
+	cout << "a : " << a << endl;
+	cout << "b : " << b << endl;
+	cout << "c : " << c << endl;
+	cout << "d : " << d << endl;
+
+	NaryBigNum e = a + b;
+	cout << "a + b : " << e << endl;
+
+	NaryBigNum f(a * b);
+	cout << "a * b : " << f << endl;
+
+	NaryBigNum g;
+	g = c + d;
+	cout << "c + d : " << g << endl;
+
+	cout << "c * d : " << c * d << endl;
+
+	return 0;
+}
